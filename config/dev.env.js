ad1836911db566fb5432aa8b98031e8d5e658707
Kai-Mikael Alanne <kaitsu@futurice.com>
2017-04-19 12:59:51 +0300

    Cleaning

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,6 +3,6 @@ var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  AUTH0_CLIENT_ID: '"I2wuPB35AiGPXoCMX5k0EnJE3nMU5GGB"',
-  AUTH0_DOMAIN: '"kmalanne.eu.auth0.com"'
+  AUTH0_CLIENT_ID: '',
+  AUTH0_DOMAIN: ''
 })
